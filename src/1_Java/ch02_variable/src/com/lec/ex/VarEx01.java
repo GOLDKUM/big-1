34ffd780c2ccc3e36bf6de6b6712e4faed498380
Soyoung-Yi <spacenyi@naver.com>
2020-11-30 17:30:44 +0900

    11-30

---
+++ src/1_Java/ch02_variable/src/com/lec/ex/VarEx01.java
@@ -0,0 +1,13 @@
+package com.lec.ex;
+public class VarEx01 {
+	public static void main(String[] args) {
+		int age = 49; // int형 age변수를 메모리공간에 마련하고
+		              // 그 age에 26을 담는다(할당).
+		String name = "장동건";
+		System.out.println(age+"살 "+name+"씨");
+		System.out.println(age+"살 "+name+"씨");
+		System.out.println(age+"살 "+name+"씨");
+		System.out.println(age+"살 "+name+"씨");
+		System.out.println(age+"살 "+name+"씨");
+	}
+}
