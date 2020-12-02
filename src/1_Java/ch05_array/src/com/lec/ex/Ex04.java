504ce721977a8253bf00a49d4554f6a5846a1539
Soyoung-Yi <spacenyi@naver.com>
2020-12-02 18:31:39 +0900

    12-02

--- src/1_Java/ch05_array/src/com/lec/ex/Ex04.java
+++ src/1_Java/ch05_array/src/com/lec/ex/Ex04.java
@@ -6,5 +6,11 @@ public class Ex04 {
 				         {40,50,60} };
 		test[0][2] = 300;
 		System.out.println(test[0][2]);
+		for(int i=0 ; i<test.length ; i++) {
+			for(int j=0 ; j<test[i].length ; j++) {
+				System.out.printf("test[%d][%d] = %d\n", 
+						                 i, j,    test[i][j]);
+			}
+		}
 	}
 }
